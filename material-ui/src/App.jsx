ee9ba4bf73f69e39d40ab4d0f4271f3a4d168ad7
Gabriel Giri <gabrielgiri@gmail.com>
2021-05-10 12:19:45 -0300

    React and materialUi installation

---
+++ material-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div>
+    <h1>Material UI - React</h1>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    </div>
+  );
+}
+
+export default App;
